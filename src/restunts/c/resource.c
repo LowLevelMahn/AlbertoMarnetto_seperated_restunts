1bc41273a3a9fc0e1351249b94c16af41d0166df
calvin <calvin@9b83cfc8-dd97-b04f-a522-af082f405558>
2009-12-24 17:46:10 +0000

    c-ifyed mmgr_alloc_resmem and mmgr_free (goto ftw)

--- src/restunts/c/resource.c
+++ src/restunts/c/resource.c
@@ -20,7 +20,7 @@ extern char* aReservememoryO;
 extern char* aReservememoryOutOfMemory;
 extern char* aMemoryManagerB;
 
-extern void fatal_error();
+extern void fatal_error(char*, ...);
 extern void sub_311D5();
 
 #define pushregs()\
@@ -131,57 +131,69 @@ loc_312E9:
 endfunc:
 }
 
+void far* dos_get_psp() {
+	unsigned short resseg, resofs;
+	__asm {
+		push ds
+		mov ah, 62h
+		int 21h
+		mov resseg, ds
+		mov resofs, bx
+		pop ds
+	}
+	return MK_FP(resseg, resofs);
+}
+
+unsigned short dos_alloc(unsigned short paras) {
+	unsigned short resseg;
+	__asm {
+		mov bx, paras
+		mov ah, 48h
+		int 21h
+		mov resseg, ax
+	}
+	return resseg;
+}
+
+unsigned short dos_setblock(unsigned short blockseg, unsigned short newsize) {
+	unsigned short res;
+	__asm {
+		mov bx, newsize
+		mov es, blockseg
+		mov ah, 4ah
+		int 21h
+		mov res, bx	// bx = max blocks
+	}
+	return res;
+}
+
 void mmgr_alloc_resmem(unsigned short arg_0) {
 
-	    //arg_0 = word ptr 6
-__asm {
-    push    si
-    push    di
-    mov     ax, 3000h
-    int     21h             // DOS - GET DOS VERSION
-    mov     bx, cs
-    cmp     al, 3
-    jl      short loc_3108E
-    mov     ah, 62h
-    int     21h             // DOS - 3+ - GET PSP ADDRESS
-loc_3108E:
-    mov     pspofs, bx
-    mov     pspseg, ds
-    cmp     word_3FF82, 0
-    jnz     short loc_310CD
-    mov     bx, 64h
-    mov     ah, 48h
-    int     21h             // DOS - 2+ - ALLOCATE MEMORY
-    mov     si, resptr1
-    mov     [si+.resofs], ax
-    mov     word_3FF84, ax
-    mov     es, ax
-    mov     bx, [arg_0]
-    sub     bx, [si+.resofs]
-    mov     ah, 4Ah
-    int     21h             // DOS - 2+ - ADJUST MEMORY BLOCK SIZE (SETBLOCK)
-    mov     ah, 4Ah
-    int     21h             // DOS - 2+ - ADJUST MEMORY BLOCK SIZE (SETBLOCK)
-    mov     ax, word_3FF84
-    add     ax, bx
-    mov     si, resendptr2
-    mov     [si+.resofs], ax
-    mov     word_3FF82, ax
-loc_310CD:
-    mov     si, resendptr2
-    mov     resendptr1, si
-    mov     si, resptr1
-    mov     resptr2, si
-loc_310DD:
-    add     si, 12h
-    cmp     si, resendptr2
-    jz      short loc_310ED
-    mov     word ptr [si+.resunk], 0
-    jmp     short loc_310DD
-loc_310ED:
-    pop     di
-    pop     si
-}
+	void far* psp;
+	unsigned short maxblocks;
+	struct RESOURCE* rp;
+
+	psp = dos_get_psp();
+	pspseg = FP_SEG(psp);
+	pspofs = FP_OFF(psp);
+	
+	if (word_3FF82 == 0) {
+		resptr1->resofs = dos_alloc(0x64);
+		word_3FF84 = resptr1->resofs;
+		maxblocks = dos_setblock(resptr1->resofs, arg_0 - resptr1->resofs);
+		maxblocks = dos_setblock(resptr1->resofs, maxblocks);
+		resendptr2->resofs = word_3FF84 + maxblocks;
+		word_3FF82 = resendptr2->resofs;
+	}
+	resendptr1 = resendptr2;
+	resptr2 = resptr1;
+	
+	rp = resptr1;
+	for (;;) {
+		rp++;
+		if (rp == resendptr2) break;
+		rp->resunk = 0;
+	}
 }
 
 void mmgr_alloc_a000() {
@@ -192,75 +204,50 @@ unsigned short mmgr_get_ofs_diff() {
 	return resendptr2->resofs - resptr2->resofs - resptr2->ressize;
 }
 
-void mmgr_free(unsigned short arg_0, unsigned short arg_2) {
-	__asm {
-    push    si
-    push    di
-    mov     ax, [arg_2]
-    mov     si, resptr2
-loc_31488:
-    cmp     si, resptr1
-    jz      short loc_31498
-    cmp     ax, [si+.resofs]
-    jz      short loc_314A4
-    sub     si, 12h
-    jmp     short loc_31488
-loc_31498:
-    push    [arg_2]
-    mov     ax, offset aMemoryManagerB // "memory manager - BLOCK NOT FOUND at SEG"...
-    push    ax
-    call    far ptr fatal_error
-loc_314A4:
-    mov     [arg_2], 0
-    mov     word ptr [si+.resunk], 0
-    cmp     si, resptr2
-    jz      short loc_314CE
-    mov     bx, resptr2
-    mov     di, resendptr1
-    cmp     si, di
-    jz      short loc_31508
-    mov     ax, [di+.resofs]
-    sub     ax, [bx+.resofs]
-    sub     ax, [bx+.ressize]
-    cmp     ax, [si+.ressize]
-    jb      short loc_31508
-loc_314CE:
-    mov     bx, [si+.ressize]
-    push    bx
-    mov     di, resendptr1
-    mov     ax, [di+.resofs]
-    sub     ax, bx
-    push    ax
-    mov     [arg_2], ax
-    push    word ptr [si+.resofs]
-    sub     di, 12h
-    mov     resendptr1, di
-    mov     [di+.resofs], ax
-    mov     [di+.ressize], bx
-    mov     word ptr [di+.resunk], 1
-    mov     cx, 0Ch
-    xor     bx, bx
-loc_314F9:
-    mov     al, byte ptr [bx+si+.resname]
-    mov     byte ptr [bx+di+.resname], al
-    inc     bx
-    loop    loc_314F9
-    call    far ptr sub_311D5
-    add     sp, 6
+void far* mmgr_free(unsigned short arg_0, unsigned short arg_2) {
+	int i;
+	unsigned short ax, bx, cx, dx, di;
+	struct RESOURCE* ressi;
+	struct RESOURCE* resbx;
+
+	ressi = resptr2;
+
+	while (1) {
+		if (ressi == resptr1) 
+			fatal_error(aMemoryManagerB, arg_2);
+		if (ressi->resofs == arg_2) break;
+		ressi--;
+	}
+
+	arg_2 = 0;
+	ressi->resunk = 0;
+	if (ressi != resptr2) {
+		if (ressi == resendptr1) goto loc_31508;
+		ax = resendptr1->resofs - resptr2->resofs - resptr2->ressize;
+		if (ax < ressi->ressize) goto loc_31508;
+	}
+
+	arg_2 = resendptr1->resofs - ressi->ressize;
+	resendptr1--;
+	resendptr1->resofs = arg_2;
+	resendptr1->ressize = ressi->ressize;
+	resendptr1->resunk = 1;
+
+	for (i = 0; i < 0xC; i++) {
+		resendptr1->resname[i] = ressi->resname[i];
+	}
+
+	sub_311D5(ressi->resofs, arg_2, ressi->ressize);
+
 loc_31508:
-    cmp     si, resptr2
-    jnz     short loc_3151B
-loc_3150E:
-    sub     si, 12h
-    cmp     word ptr [si+.resunk], 0
-    jz      short loc_3150E
-    mov     resptr2, si
-loc_3151B:
-    mov     ax, [arg_0]
-    mov     dx, [arg_2]
-    pop     di
-    pop     si
-}
+	if (ressi == resptr2) {
+		do {
+			ressi--;
+		} while (ressi->resunk == 0);
+		resptr2 = ressi;
+	}
+
+	return MK_FP(arg_0, arg_2);
 }
 
 void mmgr_copy_paras(unsigned short srcseg, unsigned short destseg, short paras) {
