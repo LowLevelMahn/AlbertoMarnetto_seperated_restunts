b6b7dd6bec64d7611e22cf02e18d3502d4518597
calvin <calvin@9b83cfc8-dd97-b04f-a522-af082f405558>
2014-10-26 07:41:25 +0000

    - bat magic + fix

--- tools/bin/tasmbox.bat
+++ tools/bin/tasmbox.bat
@@ -1,13 +1,14 @@
 @echo off
 del tasm.dosbox.conf 2>nul
-copy %~dp0dosbox.tmpl.conf tasm.dosbox.conf
+copy %~dp0dosbox.tmpl.conf tasm.dosbox.conf >nul
 echo [autoexec] >>tasm.dosbox.conf
 echo MOUNT S S:\ >>tasm.dosbox.conf
 echo S: >>tasm.dosbox.conf
 echo SET PATH=%%PATH%%;s:\tools\bin >>tasm.dosbox.conf
 echo cd %CD% >>tasm.dosbox.conf
-echo tasmx %1 %2 %3 %4 %5 %6 %7 %8 %9 ^>tasmbox.out >>tasm.dosbox.conf
+echo tasmx %* ^>tasmbox.out >>tasm.dosbox.conf
 echo if ERRORLEVEL 0 goto noerror >>tasm.dosbox.conf
+echo type tasmbox.out >>tasm.dosbox.conf
 echo pause >>tasm.dosbox.conf
 echo :noerror >>tasm.dosbox.conf
 echo exit >>tasm.dosbox.conf
